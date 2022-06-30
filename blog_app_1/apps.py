0cde6c159d18bfc13e51a7ae07af4477cda01520
Pablo Gonzalez <pablozalez123@gmail.com>
2022-06-30 20:18:19 -0300

    Preparado para empezar

---
+++ blog_app_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlogApp1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog_app_1'
---
+++ blog_app_1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# vista acerca de nosotros, home y listas   (donde iria el formulario de busqueda y creacion de modelos)
